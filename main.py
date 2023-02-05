577dbd17634140bf35bc1b37b6443ff94bd2d8be
hari-bhandari <2012bhandari.ha@gmail.com>
2023-02-05 08:31:10 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def sign(number):
+    if number > 0:
+        return 1
+    if number < 0:
+        return -1
+
